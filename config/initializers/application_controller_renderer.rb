f5c0a5df940980c520ad3cc9801665058e8f6a23
yuki-yamada-man <major.in.from@i.softbank.jp>
2021-06-22 06:26:44 +0000

    First

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b2da7b6200a69b87917892af0be7667af96581dcad87040b7bcddd9f2c770e2fd1923f6a4a43c503ae4b114549ec2819b6c93f3e18ae963c6f492b19abe8bdae'
